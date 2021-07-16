c5690bdc8c7cb7f69c014b34c7713f85f807bec4
Fahim Anzam Dip <dfahimanzam@gmail.com>
2021-07-17 00:51:40 +0600

    Setup: Laravel Roles & Permissions

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
          * Package Service Providers...
          */
         RealRashid\SweetAlert\SweetAlertServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
         /*
          * Application Service Providers...
          */
