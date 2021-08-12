2fc2b07522f40a1bb8c25560dc49c12acd6122e1
Fahim <fahim@pop-os.localdomain>
2021-08-13 02:54:58 +0600

    Tweaks

--- Modules/Currency/Database/Seeders/CurrencyDatabaseSeeder.php
+++ Modules/Currency/Database/Seeders/CurrencyDatabaseSeeder.php
@@ -4,18 +4,25 @@ namespace Modules\Currency\Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Modules\Currency\Entities\Currency;
 
 class CurrencyDatabaseSeeder extends Seeder
 {
+
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
-        Model::unguard();
-
-        // $this->call("OthersTableSeeder");
+    public function run() {
+        Currency::create([
+            'currency_name'      => 'US Dollar',
+            'code'               => Str::upper('USD'),
+            'symbol'             => '$',
+            'thousand_separator' => ',',
+            'decimal_separator'  => '.',
+            'exchange_rate'      => null
+        ]);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Modules\Currency\Database\Seeders\CurrencyDatabaseSeeder;
 use Modules\Setting\Database\Seeders\SettingDatabaseSeeder;
 use Modules\User\Database\Seeders\PermissionsTableSeeder;
 
@@ -17,6 +18,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(PermissionsTableSeeder::class);
         $this->call(SuperUserSeeder::class);
+        $this->call(CurrencyDatabaseSeeder::class);
         $this->call(SettingDatabaseSeeder::class);
     }
 }
