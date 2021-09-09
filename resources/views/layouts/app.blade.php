df0391efdf3c5cc967c02c7d9e601c9ce77a6df7
Fahim <fahim@pop-os.localdomain>
2021-09-09 21:13:18 +0600

    Tweaks

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <title>@yield('title') || {{ config('app.name') }}</title>
+    <meta content="Fahim Anzam Dip" name="author">
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('images/favicon.png') }}">
 
     @include('includes.main-css')
 </head>
