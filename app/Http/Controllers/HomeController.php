a728ea0dc6a18bee91d56fbacbff3078a25ab4d1
Fahim Anzam Dip <dfahimanzam@gmail.com>
2023-07-01 14:40:10 +0600

    Fixed #11

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,7 +25,9 @@ class HomeController extends Controller
 
         foreach (Sale::completed()->with('saleDetails')->get() as $sale) {
             foreach ($sale->saleDetails??[] as $saleDetail) {
-                $product_costs += $saleDetail->product->product_cost * $saleDetail->quantity;
+                if (!is_null($saleDetail->product())) {
+                    $product_costs += $saleDetail->product->product_cost * $saleDetail->quantity;
+                }
             }
         }
 
