5830d6b096488fe84c7db2a87186fc5b1dff667b
Rickythegreat1 <134174492+Rickythegreat1@users.noreply.github.com>
2023-11-03 13:23:53 -0400

    Update repository visualization

--- repo_visualizer_gui.py
+++ repo_visualizer_gui.py
@@ -2,8 +2,9 @@
 import tkinter as tk
 from tkinter import filedialog, messagebox
 import subprocess
-import webbrowser
+import time
 import os
+import webbrowser
 
 class RepoVisualizerGUI(tk.Tk):
     def __init__(self):
@@ -38,8 +39,19 @@ class RepoVisualizerGUI(tk.Tk):
             subprocess.check_call(['git', 'commit', '-m', 'Update repository visualization'], cwd=repo_dir)
             subprocess.check_call(['git', 'push'], cwd=repo_dir)
             messagebox.showinfo('Success', 'Repository visualization updated and pushed to remote.')
+            
+            # Wait for GitHub Action to complete (this is a naive wait, GitHub Actions can take variable time)
+            time.sleep(60)  # Wait for 1 minute (adjust as necessary)
+            
+            # Pull the changes from the repository
+            subprocess.check_call(['git', 'pull'], cwd=repo_dir)
+            
+            # Assuming the SVG is named 'diagram.svg' and placed in the root of the repo
             self.svg_path = os.path.join(repo_dir, 'diagram.svg')
-            self.open_svg_button['state'] = 'normal'  # Enable the button to open the SVG
+            if os.path.exists(self.svg_path):
+                self.open_svg_button['state'] = 'normal'  # Enable the button to open the SVG
+            else:
+                messagebox.showwarning('Warning', 'SVG file not found. Check if GitHub Action completed successfully.')
         except subprocess.CalledProcessError as e:
             messagebox.showerror('Error', 'An error occurred while visualizing the repository.')
             print(e)
@@ -53,3 +65,4 @@ class RepoVisualizerGUI(tk.Tk):
 if __name__ == '__main__':
     app = RepoVisualizerGUI()
     app.mainloop()
+
